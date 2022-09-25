7ccb43a8e0a701a9a05ab47190c1f3dfb300a793
Nicholas Taveras <taveras123n@gmail.com>
2022-09-24 21:27:48 -0400

    Removed anything that has to do with animations

--- AnubisRevenge/Assets/Scripts/GamePlay/PlayerScripts/PlayerAttack.cs
+++ AnubisRevenge/Assets/Scripts/GamePlay/PlayerScripts/PlayerAttack.cs
@@ -5,10 +5,8 @@ using UnityEngine;
 public class PlayerAttack : MonoBehaviour
 {
     private PlayerInput pInput;
-    private PlayerAnimationHandler pAnimHandler;
     public GameObject projectilePrefab;
     public GameObject launchableProjectilePrefab;
-    public Animator animator;
     public Transform projectileSpawnPoint;
     public Transform launchableProjectileSpawnPoint;
 
@@ -29,54 +27,22 @@ public class PlayerAttack : MonoBehaviour
     private bool Thrown;
     private float throwDelay;
     public float throwDelayStartTime;
-    private Animator anim;
-    private GameObject player;
 
-    private bool isMeleePressed;
-    public bool isMelee;    
-    private bool isShootPressed;
-    public bool isShooting;    
-    private bool isThrowPressed;
-    public bool isThrowing;
 
     private float attackDelay;
     // Animation States
-    private const string PLAYER_CROUCHSHOOT = "Player_CrouchShoot";
-    private const string PLAYER_CROUCHTHROW = "Player_CrouchThrow";
-    private const string PLAYER_CROUCHMELEE = "Player_CrouchMelee";
-    private const string PLAYER_MELEE = "Player_Melee";
-    private const string PLAYER_SHOOT = "Player_Shoot";
-    private const string PLAYER_THROW = "Player_Throw";
-    private const string PLAYER_JUMPSHOOT = "Player_JumpShoot";
-    private const string PLAYER_JUMPTHROW = "Player_JumpThrow";
-    private const string PLAYER_JUMPMELEE = "Player_JumpMelee";
-    private const string PLAYER_WALKSHOOT = "Player_WalkShoot";
-    private const string PLAYER_RUNSHOOT = "Player_RunShoot";
+
 
     // Start is called before the first frame update
     void Start()
     {
         pInput = GetComponent<PlayerInput>();
-        pAnimHandler = GetComponent<PlayerAnimationHandler>();
-        player = GameObject.Find("PlayerCharacter");
         pCtrl = GameObject.Find("PlayerCharacter").GetComponent<PlayerController>();
-        anim = GetComponent<Animator>();
     }
 
     private void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Mouse0) && !pInput.GetisRunning() && !pInput.GetisWalking())
-        {
-            isMeleePressed = true;
-        }
-        if(Input.GetKeyDown(KeyCode.Mouse1))
-        {
-            isShootPressed = true;
-        }
-        if(Input.GetKeyDown(KeyCode.F) && !pInput.GetisRunning() && !pInput.GetisWalking())
-        {
-            isThrowPressed = true;
-        }
+
     }
 
     // Update is called once per frame
@@ -84,104 +50,38 @@ public class PlayerAttack : MonoBehaviour
     {
         // Melee
 
-        if (isMeleePressed && timeBtwMeleeAttack > startTimeMeleeBtwAttack && !pInput.GetisRunning() && !pInput.GetisWalking())
+        if (pInput.GetisMeleePressed() && timeBtwMeleeAttack > startTimeMeleeBtwAttack && !pInput.GetisRunning() && !pInput.GetisWalking())
         {
-            isMeleePressed = false;
-
-            if (!isMelee)
+            timeBtwMeleeAttack = 0;
+            Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(attackPos.position, meleeAttackRange, whatIsEnemies);
+            for (int i = 0; i < enemiesToDamage.Length; i++)
             {
-                isMelee = true;
-                timeBtwMeleeAttack = 0;
-                if (pInput.GetisCrouching())
-                {
-                    pAnimHandler.ChangeAnimationState(PLAYER_CROUCHMELEE);
-                }
-                else if (!pCtrl.GetisGrounded())
-                {
-                    pAnimHandler.ChangeAnimationState(PLAYER_JUMPMELEE);
-                }
-                else if (pInput.GetisIdle())
-                {
-                    pAnimHandler.ChangeAnimationState(PLAYER_MELEE);
-                }
-                Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(attackPos.position, meleeAttackRange, whatIsEnemies);
-                for (int i = 0; i < enemiesToDamage.Length; i++)
-                {
-                    enemiesToDamage[i].GetComponent<Health>().TakeDamage(damage);
-                }
+                enemiesToDamage[i].GetComponent<Health>().TakeDamage(damage);
             }
-
-            attackDelay = anim.GetCurrentAnimatorStateInfo(0).length;
-            Invoke("MeleeAttackComplete", attackDelay);
         }
+
         timeBtwMeleeAttack += Time.deltaTime;
 
 
 
         // Shoot
-        if (isShootPressed && timeBtwRangeAttack > startTimeRangeBtwAttack)
+        if (pInput.GetisShootPressed() && timeBtwRangeAttack > startTimeRangeBtwAttack)
         {
-            isShootPressed = false;
-            if(!isShooting)
-            {
-                isShooting = true;
-
-                timeBtwRangeAttack = 0;
-                if (pInput.GetisCrouching())
-                {
-                    pAnimHandler.ChangeAnimationState(PLAYER_CROUCHSHOOT);
-                }
-                else if (!pCtrl.GetisGrounded())
-                {
-                    pAnimHandler.ChangeAnimationState(PLAYER_JUMPSHOOT);
-                }
-                else if (pInput.GetisIdle())
-                {
-                    pAnimHandler.ChangeAnimationState(PLAYER_SHOOT);
-                }
-                else if (pInput.GetisWalking())
-                {
-                    pAnimHandler.ChangeAnimationState(PLAYER_WALKSHOOT);
-                }
-                else if (pInput.GetisRunning())
-                {
-                    pAnimHandler.ChangeAnimationState(PLAYER_RUNSHOOT);
-                }
-                Instantiate(projectilePrefab, projectileSpawnPoint.transform.position, player.transform.rotation);
-            }
-
-            attackDelay = anim.GetCurrentAnimatorStateInfo(0).length;
-            Invoke("ShootAttackComplete", attackDelay);
-
+            
+            timeBtwRangeAttack = 0;
+            Instantiate(projectilePrefab, projectileSpawnPoint.transform.position, transform.rotation);
         }
         timeBtwRangeAttack += Time.deltaTime;
 
         // Throw
-        if (isThrowPressed && timeBtwThrowAttack > startTimeThrowBtwAttack && !pInput.GetisRunning() && !pInput.GetisWalking())
+        if (pInput.GetisThrowPressed() && timeBtwThrowAttack > startTimeThrowBtwAttack && !pInput.GetisRunning() && !pInput.GetisWalking())
         {
-            isThrowPressed = false;
-            if(!isThrowing)
-            {
-                isThrowing = true;
-                timeBtwThrowAttack = 0;
-                if (pInput.GetisCrouching())
-                {
-                    pAnimHandler.ChangeAnimationState(PLAYER_CROUCHTHROW);
-                }
-                else if (!pCtrl.GetisGrounded())
-                {
-                    pAnimHandler.ChangeAnimationState(PLAYER_JUMPTHROW);
-                }
-                else if (pInput.GetisIdle())
-                {
-                    pAnimHandler.ChangeAnimationState(PLAYER_THROW);
-                }
-                Thrown = true;
-                throwDelay = 0;
+            timeBtwThrowAttack = 0;
 
-                attackDelay = anim.GetCurrentAnimatorStateInfo(0).length;
-                Invoke("ThrowAttackComplete", attackDelay);
-            }
+            Thrown = true;
+            throwDelay = 0;
+
+
         }
         timeBtwThrowAttack += Time.deltaTime;
         throwDelay += Time.deltaTime;
@@ -200,18 +100,8 @@ public class PlayerAttack : MonoBehaviour
         }
     }
 
-    void MeleeAttackComplete()
-    {
-        isMelee = false;
-    }
-    void ShootAttackComplete()
-    {
-        isShooting = false;
-    }
-    void ThrowAttackComplete()
-    {
-        isThrowing = false;
-    }
+
+
     private void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.red;
