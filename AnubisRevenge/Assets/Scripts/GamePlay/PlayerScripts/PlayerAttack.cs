5034049665687740c9ba32287883aab5bbe7f4f4
Nicholas Taveras <taveras123n@gmail.com>
2022-09-25 13:49:18 -0400

    Moved timers to PlayerTimeManager.cs and added comments and removed unnecessary variables

--- AnubisRevenge/Assets/Scripts/GamePlay/PlayerScripts/PlayerAttack.cs
+++ AnubisRevenge/Assets/Scripts/GamePlay/PlayerScripts/PlayerAttack.cs
@@ -5,102 +5,52 @@ using UnityEngine;
 public class PlayerAttack : MonoBehaviour
 {
     private PlayerInput pInput;
-    public GameObject projectilePrefab;
-    public GameObject launchableProjectilePrefab;
-    public Transform projectileSpawnPoint;
-    public Transform launchableProjectileSpawnPoint;
 
-    private float timeBtwMeleeAttack;
-    public float startTimeMeleeBtwAttack;
+    [SerializeField] private GameObject projectilePrefab;
+    [SerializeField] private GameObject launchableProjectilePrefab;
 
-    private float timeBtwRangeAttack;
-    public float startTimeRangeBtwAttack; 
-    
-    private float timeBtwThrowAttack;
-    public float startTimeThrowBtwAttack;
+    [SerializeField] private Transform projectileSpawnPoint;
+    [SerializeField] private Transform launchableProjectileSpawnPoint;
+    [SerializeField] private Transform attackPos;
 
-    private PlayerController pCtrl;
-    public Transform attackPos;
-    public float meleeAttackRange;
-    public LayerMask whatIsEnemies;
-    public int damage;
-    private bool Thrown;
-    private float throwDelay;
-    public float throwDelayStartTime;
+    [SerializeField] private LayerMask whatIsEnemies;
 
-
-    private float attackDelay;
-    // Animation States
-
+    [SerializeField] private float meleeAttackRange;
+    [SerializeField] private float damage;
 
     // Start is called before the first frame update
     void Start()
     {
         pInput = GetComponent<PlayerInput>();
-        pCtrl = GameObject.Find("PlayerCharacter").GetComponent<PlayerController>();
     }
-
-    private void Update()
-    {
-
-    }
-
     // Update is called once per frame
     void FixedUpdate()
     {
-        // Melee
-
-        if (pInput.GetisMeleePressed() && timeBtwMeleeAttack > startTimeMeleeBtwAttack && !pInput.GetisRunning() && !pInput.GetisWalking())
+        // Melee Button Pressed?
+        if (pInput.GetisMeleePressed())
         {
-            timeBtwMeleeAttack = 0;
+            // Checks if there are any gameObjects in the whatIsEnemies LayerMask and if they are within Range
             Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(attackPos.position, meleeAttackRange, whatIsEnemies);
+            // Loops through all Game Objects that are withn Range and in the Layer Mask
             for (int i = 0; i < enemiesToDamage.Length; i++)
             {
+                // Removes Health from GameObjects that are within Range and in the LayerMask
                 enemiesToDamage[i].GetComponent<Health>().TakeDamage(damage);
             }
         }
-
-        timeBtwMeleeAttack += Time.deltaTime;
-
-
-
-        // Shoot
-        if (pInput.GetisShootPressed() && timeBtwRangeAttack > startTimeRangeBtwAttack)
-        {
-            
-            timeBtwRangeAttack = 0;
+        // Shoot Button Pressed?
+        if (pInput.GetisShootPressed())
+        {  
+            // Spawns Bullet Game Object at set position and rotation
             Instantiate(projectilePrefab, projectileSpawnPoint.transform.position, transform.rotation);
         }
-        timeBtwRangeAttack += Time.deltaTime;
-
-        // Throw
-        if (pInput.GetisThrowPressed() && timeBtwThrowAttack > startTimeThrowBtwAttack && !pInput.GetisRunning() && !pInput.GetisWalking())
+        // Throw Button Pressed
+        if (pInput.GetisThrowPressed())
         {
-            timeBtwThrowAttack = 0;
-
-            Thrown = true;
-            throwDelay = 0;
-
-
-        }
-        timeBtwThrowAttack += Time.deltaTime;
-        throwDelay += Time.deltaTime;
-        if(Thrown == true && throwDelay > throwDelayStartTime)
-        {
-            if(pCtrl.facingRight == true)
-            {
-            Instantiate(launchableProjectilePrefab, launchableProjectileSpawnPoint.position, 
-                Quaternion.Euler(0, 0, -38));
-            } else
-            {
-                Instantiate(launchableProjectilePrefab, launchableProjectileSpawnPoint.position,
-                Quaternion.Euler(0, 180, -38));
-            }
-            Thrown = false;
+            // Spawns Bullet Game Object at set position and rotation
+            Instantiate(launchableProjectilePrefab, launchableProjectileSpawnPoint.position, transform.rotation);
         }
     }
-
-
 
     private void OnDrawGizmosSelected()
     {
