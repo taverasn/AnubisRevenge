c1b9ba093d6dc61450597a972caa5ae860c317ab
Nicholas Taveras <taveras123n@gmail.com>
2022-09-25 13:51:38 -0400

    All Timers from PlayerAttack.cs moved to Time Manager and Getters and Setters created for variables

--- AnubisRevenge/Assets/Scripts/GamePlay/PlayerScripts/PlayerTimeManager.cs
+++ AnubisRevenge/Assets/Scripts/GamePlay/PlayerScripts/PlayerTimeManager.cs
@@ -4,15 +4,61 @@ using UnityEngine;
 
 public class PlayerTimeManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // Timer Variables
+    private float timeBtwMeleeAttack;
+    private float timeBtwRangeAttack;
+    private float timeBtwThrowAttack;
+    // Time Delay Variables
+    [SerializeField] private float startTimeBtwMeleeAttack;    
+    [SerializeField] private float startTimeBtwRangeAttack;
+    [SerializeField] private float startTimeBtwThrowAttack;
+
+    private void FixedUpdate()
     {
-        
+        // Adding time in seconds to timer variables
+        timeBtwMeleeAttack += Time.deltaTime;
+        timeBtwRangeAttack += Time.deltaTime;
+        timeBtwThrowAttack += Time.deltaTime;
     }
 
-    // Update is called once per frame
-    void Update()
+    // Melee Attack Timer Getters/Setters
+    public float GetTimeBtwMeleeAttack()
     {
-        
+        return timeBtwMeleeAttack;
+    }    
+    public float GetStartTimeBtwMeleeAttack()
+    {
+        return startTimeBtwMeleeAttack;
+    }    
+    public void SetTimeBtwMeleeAttack(float _timeBtwMeleeAttack)
+    { 
+        timeBtwMeleeAttack = _timeBtwMeleeAttack;
     }
+    // Range Attack Timer Getters/Setters
+    public float GetTimeBtwRangeAttack()
+    {
+        return timeBtwRangeAttack;
+    }    
+    public float GetStartTimeBtwRangeAttack()
+    {
+        return startTimeBtwRangeAttack;
+    }    
+    public void SetTimeBtwRangeAttack(float _timeBtwRangeAttack)
+    {
+        timeBtwRangeAttack = _timeBtwRangeAttack;
+    }
+    // Throw Attack Timer Getters/Setters
+    public float GetTimeBtwThrowAttack()
+    {
+        return timeBtwThrowAttack;
+    }    
+    public float GetStartTimeBtwThrowAttack()
+    {
+        return startTimeBtwThrowAttack;
+    }    
+    public void SetTimeBtwThrowAttack(float _timeBtwThrowAttack)
+    {
+        timeBtwThrowAttack = _timeBtwThrowAttack;
+    }
+
 }
