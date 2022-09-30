1429edb71118517be690dbe6d47534f5b7ddafc7
Nicholas Taveras <taveras123n@gmail.com>
2022-09-30 18:37:09 -0400

    Added Collision Detection for ladders

--- AnubisRevenge/Assets/Scripts/GamePlay/PlayerScripts/PlayerCollisions.cs
+++ AnubisRevenge/Assets/Scripts/GamePlay/PlayerScripts/PlayerCollisions.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class PlayerCollisions : MonoBehaviour
 {
     private PlayerController pCtrl;
-    internal bool canClimb;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,34 +16,17 @@ public class PlayerCollisions : MonoBehaviour
     {
 
     }
+    public bool isClimbing()
+    {
+        RaycastHit2D hit = Physics2D.BoxCast(pCtrl.capCollider.bounds.center, pCtrl.capCollider.bounds.size,
+            0, Vector2.one, 0.1f, pCtrl.climbLayer);
+        return hit.collider != null;
+    }
     public bool isGrounded()
     {
         RaycastHit2D hit = Physics2D.BoxCast(pCtrl.boxCollider.bounds.center, pCtrl.boxCollider.bounds.size,
             0, Vector2.down, 0.1f, pCtrl.groundLayer);
         return hit.collider != null;
-    }
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-/*        if (other.gameObject.tag == "Ground")
-        {
-            isGrounded = true;
-        }
-        else
-        {
-            isGrounded = false;
-        }
-        if (other.gameObject.tag == "Ladder")
-        {
-            canClimb = true;
-            pCtrl.rb.constraints = RigidbodyConstraints2D.FreezePositionY;
-            pCtrl.rb.constraints = RigidbodyConstraints2D.FreezeRotation;
-        }
-        else
-        {
-            canClimb = false;
-            pCtrl.rb.constraints = RigidbodyConstraints2D.None;
-            pCtrl.rb.constraints = RigidbodyConstraints2D.FreezeRotation;
-        }*/
     }
     private void OnCollisionEnter2D(Collision2D other)
     {
