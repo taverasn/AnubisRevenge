ce637712d59d6906242056fa9bc64a033679fab8
Shem-aiah <SINelson@student.fullsail.edu>
2022-10-05 10:35:49 -0400

    Modified Scorpion prefab and Health

--- AnubisRevenge/Assets/Scripts/GamePlay/Health/Health.cs
+++ AnubisRevenge/Assets/Scripts/GamePlay/Health/Health.cs
@@ -31,7 +31,7 @@ public class Health : MonoBehaviour
         if (dead == true && gameObject.tag == "Enemy")
         {
             startFading();
-            Destroy(gameObject.transform.parent.gameObject, 2.5f);
+            Destroy(gameObject, 2.5f);
         }
     }
 
