cd28c4fd7a8699c152cc38c5868292d37e5b3a48
Nicholas Taveras <taveras123n@gmail.com>
2022-10-11 00:00:19 -0400

    Added arcMultiplier and throw Distance multiplier to create a variable dynamite throw

--- AnubisRevenge/Assets/Scripts/GamePlay/Projectile.cs
+++ AnubisRevenge/Assets/Scripts/GamePlay/Projectile.cs
@@ -3,36 +3,36 @@ using UnityEngine.UI;
 public class Projectile : MonoBehaviour
 {
     // Component Variables
-    public PlayerController pCtrl;
     private Animator anim;
     private Rigidbody2D rb;
     // Speed and Direction Variables
     [SerializeField] private float speed;
     private float movementSpeed;
     private Vector3 direction;
+    [SerializeField] float arcMultiplier;
 
     // Damage Variable
     public int damage;
 
+
     private void Start()
     {
-        pCtrl = GameObject.Find("PlayerCharacter").GetComponent<PlayerController>();
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
         // Tag Dynamite?
         // Causes Dynamite to move in a parabola depnding on direction player is facing
         if(gameObject.tag == "Dynamite")
         {
-            if(pCtrl.pMove.facingRight)
+            if(gameManager.instance.pCtrl.pMove.facingRight)
             {
                 // Set Dynamite Rotation
                 transform.rotation = Quaternion.Euler(0, 0, -37.66f);
-                direction = transform.right + (Vector3.up * 1.5f);
+                direction = transform.right + ((Vector3.up * arcMultiplier) * (1.5f * gameManager.instance.pCtrl.pInput.throwMultiplierTimer));
             }
             else
             {
                 transform.rotation = Quaternion.Euler(0, 0, 37.66f);
-                direction = -transform.right + (Vector3.up * 1.5f);
+                direction = -transform.right + ((Vector3.up * arcMultiplier) * (1.5f * gameManager.instance.pCtrl.pInput.throwMultiplierTimer));
             }
             rb.AddForce(direction * speed, ForceMode2D.Impulse);
         }
@@ -40,7 +40,7 @@ public class Projectile : MonoBehaviour
         // Causes bullet to move in a straight line depending on direction player is facing
         if(gameObject.tag == "Bullet")
         {
-            if(pCtrl.pMove.facingRight)
+            if(gameManager.instance.pCtrl.pMove.facingRight)
                 movementSpeed = speed;
             else
                 movementSpeed = -speed;
