1ff425783fd612fe8ac6ba1d79538f8c82eada25
Nicholas Taveras <taveras123n@gmail.com>
2022-10-18 21:01:56 -0400

    Changed all the audio sources to audio clips and the sound manager now only uses one sounds source

--- AnubisRevenge/Assets/Scripts/GamePlay/soundManager.cs
+++ AnubisRevenge/Assets/Scripts/GamePlay/soundManager.cs
@@ -4,27 +4,24 @@ using UnityEngine;
 
 public class soundManager : MonoBehaviour
 {
+    [SerializeField] public AudioSource aud;
     [Header("----- Player Sounds -----")]
-    [SerializeField] public AudioSource jump;
-    [SerializeField] public AudioSource hurt;
-    [SerializeField] public AudioSource walk;
-    [SerializeField] public AudioSource run;
-    [SerializeField] public AudioSource crouch;
-    [SerializeField] public AudioSource fall;
-    [SerializeField] public AudioSource dead;
+    [SerializeField] public AudioClip jump;
+    [SerializeField] public AudioClip hurt;
+    [SerializeField] public AudioClip walk;
+    [SerializeField] public AudioClip fall;
+    [SerializeField] public AudioClip dead;
     [Header("----- Music Sounds -----")]
-    [SerializeField] public AudioSource levelMusic;
-    [SerializeField] public AudioSource hubMusic;
-    [SerializeField] public AudioSource bossMusic;
-    [Header("----- Weapons Sounds -----")]
-    [SerializeField] public AudioSource meleeSwing;
-    [SerializeField] public AudioSource dynamiteThrow;
-    [SerializeField] public AudioSource meleeHit;
-    [SerializeField] public AudioSource bulletFlying;
-    [SerializeField] public AudioSource dynamiteInMotion;
-    [SerializeField] public AudioSource dynamite;
-    [SerializeField] public AudioSource dynamiteHit;
-    [SerializeField] public AudioSource bulletHit;
-    [SerializeField] public AudioSource shoot;
+    [SerializeField] public AudioClip levelMusic;
+    [SerializeField] public AudioClip hubMusic;
+    [SerializeField] public AudioClip bossMusic;
+    [Header("----- Weapons SoundsClip")]
+    [SerializeField] public AudioClip meleeSwing;
+    [SerializeField] public AudioClip dynamiteThrow;
+    [SerializeField] public AudioClip meleeHit;
+    [SerializeField] public AudioClip dynamite;
+    [SerializeField] public AudioClip dynamiteHit;
+    [SerializeField] public AudioClip bulletHit;
+    [SerializeField] public AudioClip shoot;
 
 }
