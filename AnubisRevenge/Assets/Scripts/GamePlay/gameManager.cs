631d30c4bb63b9dfc1d40c6997dff0a247d89f60
Nicholas Taveras <taveras123n@gmail.com>
2022-10-24 17:56:29 -0400

    Added components for menus and spawn position and created function to handle paused and unpaused game states

--- AnubisRevenge/Assets/Scripts/GamePlay/gameManager.cs
+++ AnubisRevenge/Assets/Scripts/GamePlay/gameManager.cs
@@ -7,17 +7,27 @@ public class gameManager : MonoBehaviour
     public static gameManager instance;
 
     public soundManager soundManager;
+    [Header("----- Player Components -----")]
     public GameObject player;
     public PlayerController pCtrl;
 
+    [Header("----- UI Components -----")]
+    public GameObject pauseMenu;
+    public GameObject playerDeadMenu;
+    public GameObject winMenu;
+    public GameObject currentMenu;
+    public GameObject spawnPosition;
     public HealthBar healthBar;
     public ShootLimitedProjectile limitedProjectile;
+
+    public bool isPaused;
     // Start is called before the first frame update
     private void Awake()
     {
         instance = this;
         player = GameObject.FindGameObjectWithTag("Player");
         pCtrl = player.GetComponent<PlayerController>();
+        spawnPosition = GameObject.FindGameObjectWithTag("Spawn Position");
         soundManager.aud.PlayOneShot(soundManager.levelMusic, soundManager.levelMusicVol);
 
     }
@@ -25,5 +35,38 @@ public class gameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetButtonDown("Cancel") && !playerDeadMenu.activeSelf && !winMenu.activeSelf)
+        {
+            isPaused = !isPaused;
+            pauseMenu.SetActive(isPaused);
+            if (isPaused)
+                cursorLockPause();
+            else
+                cursorUnLockUnPause();
+        }
+    }
+
+    public void disablePlayer()
+    {
+        pCtrl.pMove.enabled = isPaused;
+        pCtrl.pAttack.enabled = isPaused;
+        pCtrl.pInput.enabled = isPaused;
+        pCtrl.pAnimHandler.enabled = isPaused;
+        pCtrl.pColl.enabled = isPaused;
+        pCtrl.enabled = isPaused;
+
+    }
+
+    public void cursorLockPause()
+    {
+        Time.timeScale = 0;
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.Confined;
+    }
+    public void cursorUnLockUnPause()
+    {
+        Time.timeScale = 1;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 }
