48e6f6e70e02c94ed970a55025c1d30adc6f642f
Nicholas Taveras <taveras123n@gmail.com>
2022-10-24 17:54:48 -0400

    Added Respawn function and updatePlayerHud function
    Respawn disables the dead menu and resets the gameOver bool and the players hp and current animation and position

--- AnubisRevenge/Assets/Scripts/GamePlay/PlayerScripts/PlayerController.cs
+++ AnubisRevenge/Assets/Scripts/GamePlay/PlayerScripts/PlayerController.cs
@@ -14,16 +14,17 @@ public class PlayerController : MonoBehaviour, IDamage
     [SerializeField] internal AudioSource stepsAud;
     [SerializeField] internal LayerMask groundLayer;
     [SerializeField] internal LayerMask climbLayer;
-    internal float xAxis;
-    internal float yAxis;
-
-    [Header("----- Player Stat -----")]
-    [SerializeField] internal int HP;
-
     internal Animator anim;
     internal Rigidbody2D rb;
     internal CapsuleCollider2D capCollider;
     internal BoxCollider2D boxCollider;
+
+    [Header("----- Player Stat -----")]
+    [SerializeField] internal int HP;
+
+    internal int HPOrig;
+    internal float xAxis;
+    internal float yAxis;
     internal bool isDamaged;
     internal bool gameOver;
     bool playingSteps;
@@ -33,6 +34,8 @@ public class PlayerController : MonoBehaviour, IDamage
         capCollider = GetComponent<CapsuleCollider2D>();
         anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
+        HPOrig = HP;
+        respawn();
     }
     void Start()
     {
@@ -44,7 +47,22 @@ public class PlayerController : MonoBehaviour, IDamage
         yAxis = Input.GetAxisRaw("Vertical") * .1f;
         StartCoroutine(PlaySteps());
     }
-    
+
+    public void updatePlayerHUD()
+    {
+        gameManager.instance.healthBar.SetHealth(HP);
+    }
+
+    public void respawn()
+    {
+        gameManager.instance.playerDeadMenu.SetActive(false);
+        gameOver = false;
+        pAnimHandler.currentState = "Player_Idle";
+        HP = HPOrig;
+        updatePlayerHUD();
+        transform.position = gameManager.instance.spawnPosition.transform.position;
+    }
+
     IEnumerator PlaySteps()
     {
         if (Mathf.Abs(xAxis) > 0.03f && pColl.isGrounded() && !playingSteps)
@@ -63,7 +81,7 @@ public class PlayerController : MonoBehaviour, IDamage
     public void takeDamage(int dmg)
     {
         HP -= dmg;
-        gameManager.instance.healthBar.SetHealth(HP);
+        updatePlayerHUD();
         if(HP > 0)
         {
             aud.PlayOneShot(gameManager.instance.soundManager.hurt, gameManager.instance.soundManager.hurtVol);
